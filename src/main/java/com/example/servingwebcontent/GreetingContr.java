8841c8eddb0cd1b74e55b2e645918fadae9d266c
rmgoo <rm.gooogle@gmail.com>
2020-02-26 19:53:15 +0300

    MySQL support added

--- src/main/java/com/example/servingwebcontent/GreetingContr.java
+++ src/main/java/com/example/servingwebcontent/GreetingContr.java
@@ -1,17 +1,59 @@
 package com.example.servingwebcontent;
 
+import com.example.servingwebcontent.domain.Message;
+import com.example.servingwebcontent.repos.MessageRep;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class GreetingContr {
-    @GetMapping("/greeting")
-    public String greeting (@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    @Autowired
+    private MessageRep messageRep;
 
-        model.addAttribute("name", name);
+    @GetMapping("/greeting")
+    public String greeting (
+            @RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
+        model.put("name",name);
         return "greeting";
     }
+
+    @GetMapping
+    public String main(Map<String, Object> model){
+        Iterable<Message> messages = messageRep.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public  String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRep.save(message);
+
+        Iterable<Message> messages = messageRep.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRep.findByTag(filter);
+        } else {
+            messages = messageRep.findAll();
+        }
+
+        model.put("messages", messages);
+
+        return "main";
+    }
+
 
 }
---
+++ src/main/java/com/example/servingwebcontent/repos/MessageRep.java
@@ -0,0 +1,10 @@
+package com.example.servingwebcontent.repos;
+
+import com.example.servingwebcontent.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRep extends CrudRepository<Message, Integer> {
+    List<Message> findByTag(String tag);
+}
